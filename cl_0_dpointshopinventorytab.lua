608829b0977f24e0ee2eedd6117dbe944e943a14
Instinkt <49795383+Instinkt-Servers@users.noreply.github.com>
2021-12-23 11:04:22 +0100

    Create cl_0_dpointshopinventorytab.lua
    Replace pointshop2\lua\ps2\client\tabs\inventory_tab\cl_0_dpointshopinventorytab.lua with this file.

---
+++ cl_0_dpointshopinventorytab.lua
@@ -0,0 +1,26 @@
+Pointshop2.InventoryPanels = { }
+Pointshop2.InventoryPanels2 = {	}
+function Pointshop2:AddInventoryPanel( label, icon, controlName, shouldShow )
+	table.insert( Pointshop2.InventoryPanels, { label = label, icon = icon, controlName = controlName, shouldShow = shouldShow } )
+end
+ 
+ 
+local PANEL = {}
+ 
+function PANEL:Init( )
+	for k, btnInfo in pairs( Pointshop2.InventoryPanels ) do
+		if btnInfo.shouldShow and not btnInfo.shouldShow() then
+			continue
+		end
+ 
+		local panel = vgui.Create( btnInfo.controlName )
+		table.insert(Pointshop2.InventoryPanels2, {panel = panel, controlName = btnInfo.controlName})
+		self:addMenuEntry( btnInfo.label, btnInfo.icon, panel )
+	end
+end
+ 
+Derma_Hook( PANEL, "Paint", "Paint", "PointshopInventoryTab" )
+derma.DefineControl( "DPointshopInventoryTab", "", PANEL, "DPointshopMenuedTab" )
+ 
+Pointshop2:AddTab( "Inventory", "DPointshopInventoryTab" )
+ 
